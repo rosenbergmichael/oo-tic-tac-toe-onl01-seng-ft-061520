e16539edd34afea9d197ce320cb0bbebfbd14c2e
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-26 00:21:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -84,7 +84,7 @@ class TicTacToe
   
   def play 
    turn until over? 
-   puts winner ? "Congratulations #{winner}!" : "Cat's game!"
+   puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
   end   
   
 end   
